8796d291fc2b0edc063fa99a340acfbe18f962fa
Raphael Salviano <raphael_salviano@outlook.com>
2024-05-19 11:10:37 -0300

    feat(stages): add $replaceRoot stage

---
+++ src/stages/replaceRoot.ts
@@ -0,0 +1,20 @@
+import type { DefaultObject } from '../types';
+import type Realm from 'realm';
+
+export function replaceRootStage(
+  collection: DefaultObject[],
+  pipeline: any,
+  realm: Realm
+) {
+  return collection.map((item) => {
+    const [[key, value]] = Object.entries(pipeline);
+
+    if (!key || key !== 'newRoot')
+      throw new Error('Invalid sintax $replaceRoot');
+    if (!value) throw new Error('Invalid value');
+    if (typeof value !== 'string')
+      throw new Error('Expression model not implemented');
+
+    return item[value.slice(1)] as DefaultObject;
+  });
+}
