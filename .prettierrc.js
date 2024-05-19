ab6f140607e6edb9512260f2621d50fe658b5aa0
Raphael Salviano <raphael_salviano@outlook.com>
2024-05-19 10:14:09 -0300

    chore: add prettier configuration to the project

---
+++ .prettierrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+  semi: true,
+  singleQuote: true,
+  quoteProps: 'consistent',
+  trailingComma: 'none',
+  bracketSameLine: true,
+  bracketSpacing: true,
+  arrowParens: 'always',
+  jsxBracketSameLine: true,
+  jsxSingleQuote: true,
+  endOfLine: 'auto'
+};
